e7b0ce2d1c247cb46330e9263e8c723431e8f2f9
NiCHUY <romannichiporukk@gmail.com>
2023-08-06 20:16:29 +0300

    Singleton is implemented.

---
+++ src/Singleton.java
@@ -0,0 +1,13 @@
+//Normal implementation
+public class Singleton {
+    private static Singleton instance;
+    private Singleton(){
+        //something
+    }
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
